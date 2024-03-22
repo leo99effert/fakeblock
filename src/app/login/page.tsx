aefaac1a6349f23e4ac357a469656ed428817d5f
leo99effert <leo99effert@outlook.com>
2024-03-22 21:18:11 +0100

    color and blank pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1>Login</h1>
+    </div>
+  );
+}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import logo from "../../public/logo.jpg";
+import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="bg-black sticky top-0 font-bold">
+    <nav className="bg-blue-800 sticky top-0 font-bold">
       <ul className="h-14 flex justify-evenly items-center text-3xl">
-        <li>Profile</li>
-        <li>Login</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link href="/login">Login</Link>
+        </li>
       </ul>
     </nav>
   );
