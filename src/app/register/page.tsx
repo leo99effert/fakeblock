982b1b29ec031a262afa16de00764362857b44c7
leo99effert <leo99effert@outlook.com>
2024-03-23 12:57:52 +0100

    Register page work in progress

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,9 @@
 export default function Register() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h1>Register</h1>
+      <div className="bg-gradient-to-b from-gray-400 to-blue-400 text-blue-950 font-bold text-3xl p-20 rounded shadow-2xl">
+        Register
+      </div>
     </div>
   );
 }
